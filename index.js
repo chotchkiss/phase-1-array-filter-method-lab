08b36e90b6b593eeacced426a44639af9824fd3f
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-11-02 05:22:52 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, charsToStartWith){
+    return drivers.filter((driver) => driver.startsWith(charsToStartWith))
+}
+
+function matchName(drivers, name){
+    return drivers.filter((driver) => driver.name === name)
+
+}
